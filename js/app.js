232ed3cf53cde99536cc41569087381097f1ed55
Sander Brand <sander.brand@icasting.com>
2014-09-17 10:52:49 +0200

    .

--- js/app.js
+++ js/app.js
@@ -1,16 +1,129 @@
-var app = angular.module('addToCalendar', [])
+var app = angular.module('app', [])
     .controller('mainCtrl', [
         '$scope',
         function($scope) {
 
-
+            $scope.hello = 'hello world';
+            $scope.formDummies =
+                $scope.success = function(response) {
+                    console.log("Form Response: Success");
+                };
+            $scope.failure = function(response) {
+                console.log("Form Reponse: Fail: " + response);
+            };
             $scope.event = {
                 start: '06/19/2014 14:42:30',
                 end: '06/23/2014 14:45:00',
-                title: 'Some event',
+                title: 'Casten bij iCasting',
                 address:'Hoofdstraat 28, Zwolle, Nederland',
                 description: 'Short description'
             };
 
         }
-]);
+    ]).directive('addtocalendar', ['$compile',function($compile) {
+        return {
+            restrict: 'A',
+            scope: true,
+            templateUrl:'/template/partials/addToCalender.html',
+            link: function(scope,element){
+              scope.calendars = {};
+              var MS_IN_MINUTES = 60 * 1000;
+                var event = scope.event;
+                event.start = new Date(event.start);
+                event.end = new Date(event.end);
+                var formatTime = function(date) {
+                  date = new Date(date);
+                  return date.toISOString().replace(/-|:|\.\d+/g, '');
+                };
+    
+                var calculateEndTime = function(event) {
+                  return event.end ?
+                    formatTime(event.end) :
+                    formatTime(new Date(event.start.getTime() + (event.duration * MS_IN_MINUTES)));
+                };            
+                var calendarGenerators = {
+                    google: function(event) {
+                        var startTime = formatTime(event.start);
+                        var endTime = calculateEndTime(event);
+    
+                        var href = encodeURI([
+                            'https://www.google.com/calendar/render',
+                            '?action=TEMPLATE',
+                            '&text=' + (event.title || ''),
+                            '&dates=' + (startTime || ''),
+                            '/' + (endTime || ''),
+                            '&details=' + (event.description || ''),
+                            '&location=' + (event.address || ''),
+                            '&sprop=&sprop=name:'
+                        ].join(''));
+                        return href;
+                    },
+    
+                    yahoo: function(event) {
+                       var eventDuration =  ((event.end.getTime() - event.start.getTime()) / MS_IN_MINUTES);
+                        // Yahoo dates are crazy, we need to convert the duration from minutes to hh:mm 
+                        var yahooHourDuration = eventDuration < 600 ?
+                            '0' + Math.floor((eventDuration / 60)) :
+                            Math.floor((eventDuration / 60)) + '';
+    
+                        var yahooMinuteDuration = eventDuration % 60 < 10 ?
+                            '0' + eventDuration % 60 :
+                            eventDuration % 60 + '';
+    
+                        var yahooEventDuration = yahooHourDuration + yahooMinuteDuration;
+    
+                        // Remove timezone from event time
+                        var st = formatTime(new Date(event.start - (event.start.getTimezoneOffset() *
+                            MS_IN_MINUTES))) || '';
+    
+                        var href = encodeURI([
+                            'http://calendar.yahoo.com/?v=60&view=d&type=20',
+                            '&title=' + (event.title || ''),
+                            '&st=' + st,
+                            '&dur=' + (yahooEventDuration || ''),
+                            '&desc=' + (event.description || ''),
+                            '&in_loc=' + (event.address || '')
+                        ].join(''));
+    
+                        return  href;
+                    },
+    
+                    ics: function(event, icon, calendarName) {
+                        var startTime = formatTime(event.start);
+                        var endTime = calculateEndTime(event);
+    
+                        var href = encodeURI(
+                            'data:text/calendar;charset=utf8,' + [
+                                'BEGIN:VCALENDAR',
+                                'VERSION:2.0',
+                                'BEGIN:VEVENT',
+                                'URL:' + document.URL,
+                                'DTSTART:' + (startTime || ''),
+                                'DTEND:' + (endTime || ''),
+                                'SUMMARY:' + (event.title || ''),
+                                'DESCRIPTION:' + (event.description || ''),
+                                'LOCATION:' + (event.address || ''),
+                                'END:VEVENT',
+                                'END:VCALENDAR'
+                            ].join('\n'));
+    
+                        return href;
+                    },
+    
+                    ical: function(event) {
+                        return this.ics(event, 'icon-ical', 'iCal');
+                    },
+    
+                    outlook: function(event) {
+                        return this.ics(event, 'icon-outlook', 'Outlook');
+                    }
+                }
+                 scope.calendars = [
+                                        {name: 'Google Calendar', url: calendarGenerators.google(event)},
+                                        {name: 'Yahoo Calendar', url: calendarGenerators.yahoo(event)},
+                                        {name: 'Ical', url: calendarGenerators.ical(event)},
+                                        {name:'Outlook', url: calendarGenerators.outlook(event)}
+                                      ];
+            },
+        }
+    }]);
